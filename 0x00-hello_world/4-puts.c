202567277ce47041d3bc10275d0f8dded67a65bf
root <eugenepro8>
2022-09-09 08:02:06 -0700

    coding

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
 /**
-* main - print the string in the main function 
-* 
-* description: using the main function 
-* this program prints "programming is like building a multilingual puzzle
-* Return: 0
+*main - print the string in the put function 
+*
+*description: using the main function
+*this program prints "programming is like building a multilingual puzzle
+*return: 0
 */
-int main (void)
+int main(void)
 {
-     puts("\"proogamming is like building a multilingual puzzle");
-     return (0);
+  puts("\"programming is like building a multilingual puzzle");
+return (0);
 }
